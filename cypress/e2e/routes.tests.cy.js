35fd2987fbfadb63010d189d780be4c2837d567f
nielsperdon <niels.perdon@gmail.com>
2023-06-09 12:41:03 +0200

    test dynamic route

--- cypress/e2e/routes.tests.cy.js
+++ cypress/e2e/routes.tests.cy.js
@@ -11,4 +11,14 @@ cy.visit("/");
 cy.findByRole("button", {name: /bands/i}).click();
 cy.findByRole('heading', {name: /our illustrious performers/i}).should("exist");
 })
+// dynamic routes
+it('display correct ban name for band route that existed at build time', () => {
+  cy.task("db:reset").visit("/bands/1");
+  cy.findByRole("heading", {name: /Shamrock Pete/i}).should("exist");
+});
 
+it("show right message = Could not retrieve band data: Error: band not found", () => {
+  cy.task("db:reset").visit("/bands/123545");
+  // use find by text to find text
+  cy.findByText(/Could not retrieve band data: Error: band not found/i).should("exist");
+});
