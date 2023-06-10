d7607fd7df9cb950d6ccd0476f10b97cc06a0056
PerdonCode <niels.perdon1@gmail.com>
2023-06-10 15:54:33 +0200

    testing a route with url param

--- __tests__/api/shows.test.cy.ts
+++ __tests__/api/shows.test.cy.ts
@@ -1,10 +1,11 @@
 // ESM
 import { testApiHandler } from 'next-test-api-route-handler';
 // Import the handler under test from the pages/api directory
-import showsHandler from 'next';
+import showsHandler from '@/pages/shows/index';
+import showsIdHandler from '@/pages/shows/[showId]';
 import { readFakeData } from '../__mocks__/fakeData';
 
-test("/api/shows returns shows from db", async () => {
+test(" get /api/shows returns shows from db", async () => {
   await testApiHandler({
     handler: showsHandler,
     test: async ({ fetch }) => {
@@ -20,3 +21,19 @@ test("/api/shows returns shows from db", async () => {
   });
 })
   
+test("get /api/shows/[showsId] returns the data for the correct show ID", async () => {
+await testApiHandler({
+  handler: showsIdHandler,
+  paramsPatcher: (params) => {
+    params.showId = 0;
+  },
+  test: async({fetch}) =>{
+    const res = await fetch({method: "GET"});
+    expect(res.status).toBe(200);
+
+    const json = await res.json();
+    const {fakeShows} = await readFakeData();
+    expect(json).equal({show: fakeShows[0]})
+  },
+});
+});
