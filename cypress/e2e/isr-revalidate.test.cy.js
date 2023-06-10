3ca3ba8959493252fe139f4e9ff7aed5c3a0a7da
PerdonCode <niels.perdon1@gmail.com>
2023-06-10 10:54:46 +0200

    revalidate isr chache

--- cypress/e2e/isr-revalidate.test.cy.js
+++ cypress/e2e/isr-revalidate.test.cy.js
@@ -1,6 +1,8 @@
 import { response } from "msw";
 import { generateNewBand } from "../../__tests__/__mocks__/fakeData/newBand"
+import { generateNewShow} from "../../__tests__/__mocks__/fakeData/newShow"
 import { generateRandomId } from "../../lib/features/reservations/utils"
+import { findByRole } from "@testing-library/react";
 
 it("should load refreshed page from cache after new vand is added", () => {
   // check that new band is not on page
@@ -28,4 +30,22 @@ it("should load refreshed page from cache after new vand is added", () => {
   // reset ISR cache to initial db conditions
   cy.resetDbAndClearIsrCache();
 
+})
+
+it("should load refreshed page from cache after new show is added", () => {
+  // check that new show is not on page
+  cy.task("db:reset").visit("/shows");
+  cy.findByRole("heading", { name: /avalanche of cheese/i}).should("not exist");
+
+  cy.request("POST", `/api/shows?secret=${secret}`, {newShow: show}).then((response) => {
+    // revalidate json resp equal to true
+    expect(response.body.revalidated).to.equal(true);
+  });
+
+  // reload page new shows should exist
+  cy.reload();
+  cy.findByRole("heading", { name: /avalanche of cheese/i}).should("exist");
+
+  // reset ISR cache to initial db conditions
+  cy.resetDbAndClearIsrCache();
 })
